d3f9db3faa918bbd95c8db0904e737ec2a503af5
SuyenRojas <suyenrojas@outlook.com>
2016-07-06 12:05:50 -0600

    Add files via upload

---
+++ Memento/src/Caretaker.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Caretaker {
+	
+	private ArrayList<Memento> articulosGuardados= new ArrayList<>();
+	
+	public void addMemento(Memento newMemento){
+		articulosGuardados.add(newMemento);
+	}
+	
+	public Memento recuperarMemento(int index){
+		return articulosGuardados.get(index);
+	}
+
+}
---
+++ Memento/src/Memento.java
@@ -0,0 +1,16 @@
+
+public class Memento {
+	
+	private String articulo;
+	
+	public Memento(String articuloNuevo) {
+		articulo = articuloNuevo;
+	}
+
+	public String getArticuloGuardado() {
+		return articulo;
+	}
+	
+	
+
+}
